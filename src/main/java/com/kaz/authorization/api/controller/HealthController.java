d7038b0937bbe4bb4ebcbfc93a890deb77ae6032
fabiokaz <fabiokaz@gmail.com>
2025-06-17 14:52:37 -0300

    refactoring

--- src/main/java/com/kaz/authorization/api/controller/HealthController.java
+++ src/main/java/com/kaz/authorization/api/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.kaz.authorization.api;
+package com.kaz.authorization.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/kaz/authorization/api/controller/HealthControllerTest.java
+++ src/test/java/com/kaz/authorization/api/controller/HealthControllerTest.java
@@ -1,4 +1,4 @@
-package com.kaz.authorization;
+package com.kaz.authorization.api.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
